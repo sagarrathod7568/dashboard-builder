7a4822b86e7437ed1eecb9bf40f55efaa1752668
sagarrathod7568 <sagarrathod050@gmail.com>
2026-06-27 17:54:32 +0530

    update api endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "",
+  baseURL: "https://dashboard-builder-k1f6.onrender.com/api",
 });
 
 export const saveLayout = (data) => API.post("/save-layout.php", data);
