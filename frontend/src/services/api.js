3e4108d208132e692a14a9055437d52d37e69e9d
sagarrathod7568 <sagarrathod050@gmail.com>
2026-06-27 16:38:40 +0530

    Test deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost/dashboard-builder/backend/api",
+  baseURL: "https://dashboard-builder-k1f6.onrender.com/api",
 });
 
-export const saveLayout = (data) => API.post("/save-layout.php", data);
-
 export const getLayout = () => API.get("/get-layout.php");
+
+export const saveLayout = (data) => API.post("/save-layout.php", data);
